396857dc23dd461e6afab26aab999faee74aac12
Xaler1 <alex.and.radchenko@gmail.com>
2024-03-13 23:07:40 -0400

    trying raw html

--- pages/3_📄_CV.py
+++ pages/3_📄_CV.py
@@ -9,4 +9,6 @@ with open("./Alexander Radchenko CV.pdf", "rb") as f:
     pdf = f.read()
 base64_pdf = base64.b64encode(pdf).decode('utf-8')
 pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="1000" type="application/x-google-chrome-pdf">'
-st.markdown(pdf_display, unsafe_allow_html=True)
+# st.markdown(pdf_display, unsafe_allow_html=True)
+alt_pdf = '<iframe src="file://./Alexander Radchenko.pdf" width="800" height="1000"></iframe>'
+st.components.v1.html(alt_pdf, width=800, height=1000)

c74107203d01fb8066ef4e4c2199e15948717f47
Xaler1 <alex.and.radchenko@gmail.com>
2024-03-13 22:51:21 -0400

    declare as x-google-chrome-pdf

--- pages/3_📄_CV.py
+++ pages/3_📄_CV.py
@@ -8,5 +8,5 @@ st.title("My CV")
 with open("./Alexander Radchenko CV.pdf", "rb") as f:
     pdf = f.read()
 base64_pdf = base64.b64encode(pdf).decode('utf-8')
-pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="1000" type="application/pdf">'
+pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="1000" type="application/x-google-chrome-pdf">'
 st.markdown(pdf_display, unsafe_allow_html=True)
